fdb5d392f9e2495b7e83738808b58fd6f762f907
Lorenzo <l.franceschini@gmail.com>
2016-09-10 12:51:49 +0200

    First Commit

---
+++ models/database.js
@@ -0,0 +1,66 @@
+/*var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', function() { client.end(); });
+*/
+
+var pg = require('pg');
+
+// create a config to configure both pooling behavior
+// and client options
+// note: all config is optional and the environment variables
+// will be read if the config is not present
+var config = {
+  user: 'lorenzo', //env var: PGUSER
+  database: 'todo', //env var: PGDATABASE
+  password: 'takion.2', //env var: PGPASSWORD
+  host: 'localhost', // Server hosting the postgres database
+  port: 5432, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+
+//this initializes a connection pool
+//it will keep idle connections open for a 30 seconds
+//and set a limit of maximum 10 idle clients
+var pool = new pg.Pool(config);
+
+// to run a query we can acquire a client from the pool,
+// run a query on the client, and then return the client to the pool
+pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+
+	//var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+
+
+  var query = pool.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)', function(err, result) {
+    //call `done()` to release the client back to the pool
+    
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    } else {
+			console.log('Query fatta con successo')
+    }
+   
+    
+    
+  });
+});
+
+pool.on('error', function (err, client) {
+  // if an error is encountered by a client while it sits idle in the pool
+  // the pool itself will emit an error event with both the error and
+  // the client which emitted the original error
+  // this is a rare occurrence but can happen if there is a network partition
+  // between your application and the database, the database restarts, etc.
+  // and so you might want to handle it and at least log it out
+  console.error('idle client error', err.message, err.stack)
+});
---
+++ routes/index.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var pg = require('pg');
+var connectionString = require('../config.js')
+
+var pgpLib = require('pg-promise');
+var monitor = require('pg-monitor');
+ 
+// pg-promise initialization options:
+var options = {
+    capTX: true, // capitalize transaction commands;
+    extend: function () {
+        // our 'notes' repository extension:
+        this.notes = repNotes(this);
+    }
+};
+ 
+monitor.attach(options); // attaching to all events;
+monitor.setTheme('matrix'); // changing default theme;
+ 
+var pgp = pgpLib(options); // initializing pg-promise;
+ 
+// instantiating the database:
+var db = pgp(connectionString);
+console.log('FATTO')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
+
+router.post('/api/v1/todos', function(req, res) {
+
+  var results = [];
+
+  // Grab data from http request
+  var dataReceived = {text: req.body.text, complete: false};
+
+  db.notes.add(dataReceived.text, dataReceived.complete)
+  .then(results => {
+      console.log("RESULTS:", results);
+      return res.json(results);
+  });
+
+});
+
+router.get('/api/v1/todos/count', function(req, res, next) {
+  var results = [];
+
+  db.notes.count()
+  .then(results => {
+      console.log("RESULTS:", results);
+      return res.json(results);
+  });
+
+});
+
+router.post('/api/v1/todos/find', function(req, res, next) {
+  var results = [];
+
+  // Grab data from http request
+  var dataReceived = {idNotes: req.body.idNotes};
+
+  db.notes.findById(dataReceived.idNotes)
+  .then(results => {
+      console.log("RESULTS:", results);
+      return res.json(results);
+  });
+});
+
+
+// Note repository
+function repNotes(obj) {
+  return {
+      
+    add: function (text, complete) {
+      return obj.one("INSERT INTO items(text, complete) VALUES($1, $2) RETURNING id",
+        [text, complete])
+        .then(data=> {
+            return data.id;
+      });
+    },
+   	// Get number of notes
+    count: function () {
+      return obj.one("SELECT count(*) FROM items")
+        .then(data=> {
+          return data.count;
+      });
+    },
+    // Find notes by id
+    findById: function (idNotes) {
+      return obj.any("SELECT text from items WHERE id = $1", [idNotes])
+        .then(data=> {
+          return data.map(m=> {
+            return m.text;
+          });
+      });
+    }
+      
+  }
+}
+
+module.exports = router;
